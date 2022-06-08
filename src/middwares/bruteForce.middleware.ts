62ab8427c98ffddc36730aa97fd68f0b2e368a50
Tolkachev <ivan.tolkachev@infourok.biz>
2022-06-08 16:24:11 +0300

    log ip3

--- src/middwares/bruteForce.middleware.ts
+++ src/middwares/bruteForce.middleware.ts
@@ -3,5 +3,6 @@ import { NextFunction, Request, Response } from 'express';
 export const bruteForceMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const ip = req.ip;
   console.log('req.ip = ', ip);
+  console.log('req.connection.remoteAddress = ', req.connection.remoteAddress);
   next();
 };
