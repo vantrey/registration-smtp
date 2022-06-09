38dc34bd79cb02aeb56239fbdf41428bd1f0c1c6
Tolkachev <ivan.tolkachev@infourok.biz>
2022-06-09 10:33:15 +0300

    add clip

--- src/middwares/bruteForce.middleware.ts
+++ src/middwares/bruteForce.middleware.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import requestIp from 'request-ip';
 
 export const bruteForceMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const ip = req.ip;
+  const clientIp = requestIp.getClientIp(req);
+  console.log('clientIp', clientIp);
   console.log('req.ip = ', ip);
   console.log('req.socket.remoteAddress = ', req.socket.remoteAddress);
   console.log('req.connection.remoteAddress = ', req.connection.remoteAddress);
